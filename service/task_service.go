14fa50a09385f43c13f2e0f3feb6c3c1bfcf5a60
RageNeko26 <rneko2006@gmail.com>
2023-06-29 00:16:21 +0700

    fix: Using repo method in service

--- service/task_service.go
+++ service/task_service.go
@@ -1,6 +1,7 @@
 package service
 
 import (
+	"errors"
 	"Sistem-Manajemen-Tugas/entity"
 	"Sistem-Manajemen-Tugas/repository"
 )
@@ -9,3 +10,11 @@ type TaskService struct {
 	Repository repository.TaskRepository
 }
 
+func (service TaskService) GetTaskById(id string) (*entity.Task, error) {
+	task := service.Repository.FindById(id)
+	if task == nil {
+		return task, errors.New("Task is not found")
+	} else {
+		return task, nil
+	}
+}
