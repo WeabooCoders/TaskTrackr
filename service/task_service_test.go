636566c6d99648150822e07aaae8fd8ca3073dd2
RageNeko26 <rneko2006@gmail.com>
2023-06-29 20:26:38 +0700

    feat: add test case for filtered data

--- service/task_service_test.go
+++ service/task_service_test.go
@@ -3,8 +3,10 @@ package service
 import (
 	"testing"
 
+	"github.com/AvinFajarF/entity"
 	"github.com/AvinFajarF/repository"
 
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 )
 
@@ -12,5 +14,16 @@ var taskRepository = &repository.TaskRepositoryMock{Mock: mock.Mock{}}
 var taskService = TaskService{Repository: taskRepository}
 
 func TestService_GetById(t *testing.T) {
-	// Todo: Add test
+	// Mock Data atau biasa disebut data tidak real. Digunakan seolah data berasal dari database
+	filteredTask := entity.Task{
+		Title:       "PR Matematika",
+		Description: "Soal Uji Kompetensi 1 - 10",
+		Status:      "Belum dikerjakan",
+		UserID:      "5",
+	}
+	taskRepository.Mock.On("FindById", "5").Return(filteredTask)
+	task, err := taskService.GetTaskById("5")
+
+	assert.NotNil(t, task)
+	assert.Nil(t, err)
 }
